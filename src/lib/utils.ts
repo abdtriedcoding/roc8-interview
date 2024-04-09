96e08ee6501ee9f15f5180401630bd915ca78f8e
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-04-09 07:06:15 +0530

    remove unused old code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
-import { jwtDecode } from "jwt-decode";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,16 +8,6 @@ export function cn(...inputs: ClassValue[]) {
 export function generateToken() {
   const token = Math.floor(Math.random() * 90000000) + 10000000;
   return token.toString();
-}
-
-// Decode the JWT token to extract user's email
-export function decodeAuthToken(authToken: string) {
-  if (authToken) {
-    const decodedToken = jwtDecode(authToken);
-    const email: string = decodedToken?.email;
-    return email;
-  }
-  return null;
 }
 
 export function getJwtSecretKey() {
